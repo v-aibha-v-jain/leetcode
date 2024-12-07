15656651e573bc2ad020d9654de7fcd05b8dff3b
v-aibha-v-jain <vjvg00@gmail.com>
2024-12-07 16:50:32 +0530

    day 58

---
+++ 141/src/App.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        System.out.println(obj.hasCycle(head));
+    }
+}
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
